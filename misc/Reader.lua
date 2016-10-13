7e22fa1add181ae45c17a9d368bbe9c51e93ade1
Ruotian Luo <rluo@ttic.edu>
2016-10-13 13:40:44 -0500

    Add reader cbp.

--- misc/Reader.lua
+++ misc/Reader.lua
@@ -38,8 +38,14 @@ function Reader:__init(opt)
   self.joint_net:add(nn.Linear(self.rnn_size, 1))
   self.joint_net:add(nn.Squeeze())
 
-  self.net = {self.vis_net, self.embed_net, self.joint_net}
+  if self.score_type == 'cbp' then
+    self.combine_net = nn.Sequential()
+    self.combine_net:add(nn.CompactBilinearPooling(self.image_l2_size + self.rnn_size))
+      :add(nn.SignedSquareRoot())
+      :add(nn.Normalize(2))
+  end
 
+  self.net = {self.vis_net, self.embed_net, self.joint_net}
 end
 
 function Reader:training()
@@ -82,7 +88,15 @@ function Reader:updateOutput(inputs)
     :expand(self.vis_feats:size(1), self.word_embed:size(2), self.vis_feats:size(2))
   self.word_embed = self.word_embed:expand(self.vis_feats:size(1), self.word_embed:size(2), self.word_embed:size(3))
 
-  self.joint_input = torch.cat({self.vis_feats, self.word_embed}, 3)
+  if self.score_type == 'cbp' then
+    self.vis_feats = self.vis_feats:contiguous()
+    self.word_embed = self.word_embed:contiguous()
+    self.joint_input = self.combine_net:forward(
+      {self.vis_feats:view(-1, self.image_l2_size), self.word_embed:view(-1, self.image_l2_size)})
+    self.joint_input = self.joint_input:view(self.vis_feats:size(1), self.word_embed:size(2), self.image_l2_size + self.rnn_size)
+  else
+    self.joint_input = torch.cat({self.vis_feats, self.word_embed}, 3)
+  end
 
   self.output = self.joint_net:forward({self.joint_input, inputs[2][2]:expand(self.vis_feats:size(1))})
   return self.output
@@ -91,8 +105,21 @@ end
 function Reader:updateGradInput(inputs, gradOutput)
   local djoint_input = self.joint_net:backward({self.joint_input, inputs[2][2]:expand(self.vis_feats:size(1))}, gradOutput)
   djoint_input = djoint_input[1]
-  local dvis_feats = djoint_input[{{},{},{1, self.vis_feats:size(2)}}]:sum(2):squeeze(2)
-  local dword_embed = djoint_input[{{},{},{self.vis_feats:size(2) + 1, -1}}]:sum(1)
+  local dvis_feats
+  local dword_embed
+  if self.score_type == 'cbp' then
+    dvis_feats, dword_embed = unpack(self.combine_net:backward(
+      {self.vis_feats:view(-1, self.image_l2_size), self.word_embed:view(-1, self.image_l2_size)},
+      djoint_input:view(-1, self.image_l2_size + self.rnn_size)))
+    dvis_feats = dvis_feats:view(self.vis_feats:size(1), self.word_embed:size(2), self.image_l2_size)
+    dword_embed = dword_embed:view(self.vis_feats:size(1), self.word_embed:size(2), self.rnn_size)
+  else
+    dvis_feats = djoint_input[{{},{},{1, self.vis_feats:size(2)}}]
+    dword_embed = djoint_input[{{},{},{self.vis_feats:size(2) + 1, -1}}]
+  end
+  dvis_feats = dvis_feats:sum(2):squeeze(2)
+  dword_embed = dword_embed:sum(1)
+  
   self.embed_net:backward(inputs[2][1], dword_embed)
   self.gradInput = {}
   self.gradInput[1] = self.vis_net:backward(inputs[1], dvis_feats)
